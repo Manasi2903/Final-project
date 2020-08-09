0f956940defb7c357c11ded4eea40ee8b59d478b
Manasi2903 <65120201+Manasi2903@users.noreply.github.com>
2020-08-09 10:29:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+/**
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import TabScreen from './src/screens/TabScreen';
+
+export default class App extends Component {
+  render() {
+    return (
+      <TabScreen/>
+    );
+  }
+}
